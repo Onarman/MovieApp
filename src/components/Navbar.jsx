96b8dfb7ea2fc040ad10a9614f1db071b3628b70
onarman <onarmannn@gmail.com>
2022-09-26 17:30:41 +0300

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    // const currentUser = {displayName:"Onarman"}
+    const currentUser = false
   return (
     <nav className="navbar navbar-expend-lg">
         <div className="container-fluid">
-            <Link to={"/"} className="navbar-brand text-white">
+            <Link to={"/"} className="navbar-brand text-light">
                 <h4>React Movie App</h4>
             </Link>
+
+            <div className="d-flex text-light align-item-center">
+                
+            {currentUser ? (
+                <>
+                <h5 className='mb-0 text-capitalize'>{currentUser.displayName}</h5>
+            <button className='ms-2 btn btn-outline-light'>Logout</button>
+                </>
+            
+            ): (
+                <>
+                <button className='ms-2 btn btn-outline-light' onClick={()=> navigate("/login")}>Login</button>
+                <button className='ms-2 btn btn-outline-light' onClick={()=> navigate("/register")}>Register</button>
+                </>
+            )} 
+            </div>
         </div>
     </nav>
   )
